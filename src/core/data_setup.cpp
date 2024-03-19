d7aba04b33d46d2044eef090a9d1a031bdfd1f81
Smanar <aurelien.pra@laposte.net>
2024-03-19 21:11:55 +0100

    Update data_setup.cpp

--- src/core/data_setup.cpp
+++ src/core/data_setup.cpp
@@ -410,11 +410,6 @@ bool HttpInitDevice(Device *d, int id)
         else if (strncmp(type, "Lighting", 8) == 0)
         {
             d->type = TYPE_LIGHT;
-
-            if (strcmp(subtype,"Impuls") == 0)
-            {
-                d->type = TYPE_PUSH;
-            }
         }
         else if (strcmp(type, "Color Switch") == 0)
         {
